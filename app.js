99635d58d8691445d4de88b02e403e7b9739ba5b
shahidsss8998-debug <shahidsss8998@gmail.com>
2026-04-25 12:45:31 +0530

    Fixed routing second time

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Log incoming requests
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
+  console.log(`${req.method} ${req.path} - Content-Type: ${req.headers['content-type'] || 'none'}`);
   next();
 });
 
